55378bc4210f4f400d857843d61306c96f8e0f00
mha1206157 <117683567+mha1206157@users.noreply.github.com>
2022-12-14 15:48:57 -0500

    Broken Currently

---
+++ MascotWars/Assets/Maria's Stuff/Scripts/StartButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtons : MonoBehaviour
+{
+
+    //Start Button
+    public one;
+    //Options Button
+    public two; 
+    //Camera Position
+    public Transform mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
