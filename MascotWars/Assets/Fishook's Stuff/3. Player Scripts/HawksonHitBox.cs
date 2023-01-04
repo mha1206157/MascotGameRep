6f9f0fea8aaf6aa88706d94ad20a93a1aed2a31f
Fishook115 <martintellezapo@icloud.com>
2023-01-04 15:37:12 -0500

    Saved scripts i guess

--- MascotWars/Assets/Fishook's Stuff/3. Player Scripts/HawksonHitBox.cs
+++ MascotWars/Assets/Fishook's Stuff/3. Player Scripts/HawksonHitBox.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HawksonHitBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
-    }
+        //if player 1 
+        if (name == "Hawkson_PrefabP1" || name == "Grizzlyn_PrefabP1")
+        {
+            //if P1 touches bacteria, then it takes 10 from health bar
+            HealthBarP2.health -= 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            //message to detect P1 collision
+            UnityEngine.Debug.Log("-10 to P1");
+        }
     }
 }
